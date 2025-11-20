d9a2a932d787cbfca1a23867978d9259a5877a84
Albert Medina <albertmecu@gmail.com>
2025-11-20 12:52:41 +0100

    fix: removed unused imports from DBConnection class

--- src/main/java/db/DBConnection.java
+++ src/main/java/db/DBConnection.java
@@ -1,8 +1,5 @@
 package db;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;

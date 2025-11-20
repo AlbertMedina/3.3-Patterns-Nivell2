2945a002b145e05d4e48213932b5f936e2e96cca
YoussefEG09 <youssef.eg09@gmail.com>
2025-11-20 10:31:40 +0100

    deleted ErrorOnIdException for no usages

--- src/main/java/exceptions/ErrorOnIdException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class ErrorOnIdException extends RuntimeException {
-    public ErrorOnIdException() {
-        super("Invalid ID, please introduce an ID that is not negative or  equal to 0.");
-    }
-}

0c73203e7a8c67eb8c1c6ba90f62eb9f85215ce3
Albert Medina <albertmecu@gmail.com>
2025-11-27 19:48:06 +0100

    feat: added method to calculate inventory value

--- src/main/java/Inventory/Inventory.java
+++ src/main/java/Inventory/Inventory.java
@@ -7,5 +7,11 @@ import room.Room;
 import java.util.List;
 
 public record Inventory(List<Room> rooms, List<Decoration> decorations, List<Hint> hints) {
-    
+
+    public double getInventoryValue() {
+        double roomsValue = rooms.stream().mapToDouble(Room::getPrice).sum();
+        double decorationsValue = decorations.stream().mapToDouble(Decoration::getValue).sum();
+        double hintsValue = hints.stream().mapToDouble(Hint::getValue).sum();
+        return roomsValue + decorationsValue + hintsValue;
+    }
 }
